0074e3ed8eb35c4f76cff5a31d818797deb1a615
FurkanÇinko <148985948+CinkoFurkan@users.noreply.github.com>
2024-10-20 14:45:54 +0300

    Bismillah

--- alibaba/settings.py
+++ alibaba/settings.py
@@ -124,7 +124,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  
+    "http://localhost:3000",
+    "https://london-client-test-1.onrender.com"  
 ]
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
